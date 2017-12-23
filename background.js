2817e57274650b08147738bc7ec6e64c7c77e609
jimmychimmyy <jimmychimmyy@gmail.com>
2017-12-23 14:23:40 -0800

    added skeleton functions to background js

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.commands.onCommand.addListener(function (command) {
 	// detect user hit Alt+C
 	// generate MLA formatted annotation to the clipboard
+	// IS NOT WORKING FOR ALL TEXTS (e.g. not working for text contained within span tag)
 	if (command === "copied") {
 		chrome.tabs.executeScript(
 				{code:"window.getSelection().toString();"},
@@ -10,4 +11,43 @@ chrome.commands.onCommand.addListener(function (command) {
 		//alert(window.getSelection().toString());
 	}
 });
+
+/*
+* Meta data we need: URL, website title, article title, date published, date accessed, author/contributor, publisher/sponser, (medium? - is source pdf, word doc, ebook, etc?)
+*
+*
+* MLA 8 citation structure: author name, title of article/page, website title, publisher name, date of publication, URL
+*
+*/
+
+// function to get contents of html page
+function getContent(url, callback) {
+	var request = new XMLHttpRequest();
+	request.onload = function() {
+		if (callback && typeof(callback) === 'function') {
+			callback(this.responseXML);
+		}
+	}
+
+	request.open('GET', url);
+	request.responseType = 'document';
+	request.send();
+}
+
+// function to get author name
+function getAuthor() {}
+
+// function to get title of article/page
+function getArticle() {}
+
+// function to website title
+function getSite() {}
+
+// function to get publisher
+function getPublisher() {}
+
+// function to get publication date
+function getPubDate() {}
+
+// we can grab the url ourselves
 
