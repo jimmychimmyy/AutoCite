7f14348a726f32742f96facf71154e568618a1a2
Tommy Chen <toc007@ucsd.edu>
2017-12-23 12:08:07 -0800

    pull commit

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+function copyToClipboard( text ){
+	var copyDiv = document.createElement('div');
+	copyDiv.contentEditable = true;
+	document.body.appendChild(copyDiv);
+	copyDiv.innerHTML = text;
+	copyDiv.unselectable = "off";
+	copyDiv.focus();
+	document.execCommand('SelectAll');
+	document.execCommand("Copy", false, null);
+	document.body.removeChild(copyDiv);
+}
+
 chrome.commands.onCommand.addListener(function (command) {
 	// detect user hit Alt+C
 	// generate MLA formatted annotation to the clipboard
@@ -5,7 +17,9 @@ chrome.commands.onCommand.addListener(function (command) {
 		chrome.tabs.executeScript(
 				{code:"window.getSelection().toString();"},
 				function(x) {
-					alert(x);
+					var res = "Copied from AutoCite: ".concat(x);
+					copyToClipboard(res);
+					alert(res);
 				});
 		//alert(window.getSelection().toString());
 	}
--- oncopy.js
+++ oncopy.js
@@ -5,3 +5,15 @@ function onCopy(e) {
 
 // register event listener
 document.addEventListener('copy', onCopy, true);
+
+function copyToClipboard( text ){
+                var copyDiv = document.createElement('div');
+                copyDiv.contentEditable = true;
+                document.body.appendChild(copyDiv);
+                copyDiv.innerHTML = text;
+                copyDiv.unselectable = "off";
+                copyDiv.focus();
+                document.execCommand('SelectAll');
+                document.execCommand("Copy", false, null);
+                document.body.removeChild(copyDiv);
+            }
