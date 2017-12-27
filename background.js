3f5c321f1023d75909f9a1605d1e06b5fdb997d6
Tommy Chen <toc007@ucsd.edu>
2017-12-26 20:31:46 -0800

    moved copy logic to oncopy.js

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+chrome.runtime.onMessage.addListener(
+		function(request, sender) {
+			if(request.action == "oncopy") {
+				alert(request.result);
+			}
+		});
 /*
  *
  */
@@ -7,114 +13,14 @@ chrome.commands.onCommand.addListener(
 			// TODO: generate MLA formatted annotation to the clipboard
 			// TODO: IS NOT WORKING FOR ALL TEXTS (e.g. not working for text contained within span tag)
 			if (command === "copied") {
-				getHLContent();
+				//getHLContent();
+				chrome.tabs.executeScript(
+						null,
+						{script:"oncopy.js"}, 
+						function () {
+						});
 			}
 			else{
 				alert("Unrecognized Command!!");
 			}
 		});
-
-/*
- * grabs the user-highlighted content
- * TODO: Add content into hello.html
- * TODO: Find a way to get the citation
- */
-function getHLContent() {
-	// inject script to grab user highlighted text
-	chrome.tabs.executeScript(
-			{code:"window.getSelection().toString();"},
-			// callback function, copies the highlighted string 
-			// into clipboard
-			function(x) {
-				if(x === undefined) 
-					alert("Could not copy highlighted text!");
-				else {
-					var res = "Copied from AutoCite: ".concat(x);
-					copyToClipboard(res);
-					chrome.browserAction.getPopup(
-							function(x) {
-								console.log(x);
-							});
-					alert(res);
-				}
-			});
-	//alert(window.getSelection().toString());
-}
-
-/*
- * copies the text into the system clipboard
- */
-function copyToClipboard( text ){
-	var copyDiv = document.createElement('div');
-	copyDiv.contentEditable = true;
-	document.body.appendChild(copyDiv);
-	copyDiv.innerHTML = text;
-	copyDiv.unselectable = "off";
-	copyDiv.focus();
-	document.execCommand('SelectAll');
-	document.execCommand("Copy", false, null);
-	document.body.removeChild(copyDiv);
-}
-
-
-/*
- * Meta data we need: 
- * 		URL, website title, article title, date published, date accessed, 
- * 		author/contributor, publisher/sponser, (medium? - is source pdf, 
- * 		word doc, ebook, etc?)
- *
- * MLA 8 citation structure: author name, title of article/page, website title,
- * 	 publisher name, date of publication, URL
- */
-
-// function to get contents of html page
-function getContent(url, callback) {
-	var request = new XMLHttpRequest();
-	request.onload = function() {
-		if (callback && typeof(callback) === 'function') {
-			callback(this.responseXML);
-		}
-	}
-
-	request.open('GET', url);
-	request.responseType = 'document';
-	request.send();
-}
-
-// function to get author name
-function getAuthor() {}
-
-// function to get title of article/page
-function getArticle() {}
-
-// function to website title
-function getSite() {}
-
-// function to get publisher
-function getPublisher() {}
-
-// function to get publication date
-function getPubDate() {}
-
-// we can grab the url ourselves
-
-// function to generate citation
-function generateCitation() {}
-
-// function to add citation to clipboard
-function toClipboard() {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- oncopy.js
+++ oncopy.js
@@ -1,7 +1,102 @@
 // on copy event, send message to background.html
 function onCopy(e) {
-	chrome.extension.sendRequest({event: "copy"});
+	chrome.extension.sendRequest(
+			{event: "copy"});
 }
 
 // register event listener
 document.addEventListener('copy', onCopy, true);
+
+chrome.runtime.sendMessage() {
+	{
+		event:"oncopy",
+		hlContent:getHLContent()
+	}
+}
+
+/*
+ * grabs the user-highlighted content
+ * TODO: Add content into hello.html
+ * TODO: Find a way to get the citation
+ */
+function getHLContent() {
+	var x = window.getSelection().toString();
+	// callback function, copies the highlighted string 
+	// into clipboard
+	if(x === undefined) 
+		alert("Could not copy highlighted text!");
+	else {
+		var res = "Copied from AutoCite: ".concat(x);
+		copyToClipboard(res);
+		chrome.browserAction.getPopup(
+				function(x) {
+					console.log(x);
+				});
+		alert(res);
+	}
+	return x
+	//alert(window.getSelection().toString());
+}
+
+/*
+ * copies the text into the system clipboard
+ */
+function copyToClipboard( text ){
+	var copyDiv = document.createElement('div');
+	copyDiv.contentEditable = true;
+	document.body.appendChild(copyDiv);
+	copyDiv.innerHTML = text;
+	copyDiv.unselectable = "off";
+	copyDiv.focus();
+	document.execCommand('SelectAll');
+	document.execCommand("Copy", false, null);
+	document.body.removeChild(copyDiv);
+}
+
+
+/*
+ * Meta data we need: 
+ * 		URL, website title, article title, date published, date accessed, 
+ * 		author/contributor, publisher/sponser, (medium? - is source pdf, 
+ * 		word doc, ebook, etc?)
+ *
+ * MLA 8 citation structure: author name, title of article/page, website title,
+ * 	 publisher name, date of publication, URL
+ */
+
+// function to get contents of html page
+function getContent(url, callback) {
+	var request = new XMLHttpRequest();
+	request.onload = function() {
+		if (callback && typeof(callback) === 'function') {
+			callback(this.responseXML);
+		}
+	}
+
+	request.open('GET', url);
+	request.responseType = 'document';
+	request.send();
+}
+
+// function to get author name
+function getAuthor() {}
+
+// function to get title of article/page
+function getArticle() {}
+
+// function to website title
+function getSite() {}
+
+// function to get publisher
+function getPublisher() {}
+
+// function to get publication date
+function getPubDate() {}
+
+// we can grab the url ourselves
+
+// function to generate citation
+function generateCitation() {}
+
+// function to add citation to clipboard
+function toClipboard() {}
